0002f2f6fd21c386490d6479dea2e882a465c3fa
김용민 <dydals3440@gmail.com>
2024-06-18 15:35:26 +0900

    feat: Setting Loading Fallback

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -7,7 +7,7 @@ export default async function NewsPage() {
 	// 클라이언트는 디비에 접근할 수 없고, 보안상의 이유로 접근해서는 안됨.
 	// react 서버 컴포넌트에서는 이 코드가 서버에서만 실행되기 떄문에 클라이언트 측에서도 실행되는 클라 컴포넌트와 달리
 	// 서버에서만 실행됩니다. 따라서 서버 측 데베에서 안전하게 접근해서 데이터를 가져올 수 있습니다.
-	const news = getAllNews();
+	const news = await getAllNews();
 
 	return (
 		<>
